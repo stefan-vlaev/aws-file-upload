b9f5fae514958482693f2b794d0f55f9bc186ed5
stefan-vlaev <svlaev@mail.bg>
2024-11-24 11:12:04 +0200

    Initial commit

---
+++ test/aws-file-upload.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsFileUpload = require('../lib/aws-file-upload-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-file-upload-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsFileUpload.AwsFileUploadStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
